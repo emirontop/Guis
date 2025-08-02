0613f4b9e35f65ed678c52f26748358ea9e392fc
emirontop <2fanisikeyim@gmail.com>
2025-08-02 23:10:28 +0300

    Create guis.js

---
+++ guis.js
@@ -0,0 +1,19 @@
+const guis = [
+  {
+    name: "Kavo UI",
+    description: "FE destekli klasik UI kütüphanesi",
+    code: `loadstring(game:HttpGet("https://raw.githubusercontent.com/Kavo-UI/Kavo-UI-Library/main/source.lua"))()`
+  },
+  {
+    name: "WindUI",
+    description: "Modern ve sade tasarıma sahip",
+    code: `loadstring(game:HttpGet("https://windhub.xyz/loader.lua"))()`
+  },
+  {
+    name: "Starlight",
+    description: "Gelişmiş tema ve animasyon destekli",
+    code: `loadstring(game:HttpGet("https://nebula-softworks.github.io/starlight.js"))()`
+  }
+];
+
+export default guis;
