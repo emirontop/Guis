01d4a9f650e3cfa9ceb5021d78be5c6a7f2a8f9a
emirontop <2fanisikeyim@gmail.com>
2025-08-02 23:18:42 +0300

    Update guis.js

--- guis.js
+++ guis.js
@@ -1,17 +1,23 @@
 const guis = [
   {
     name: "Kavo UI",
-    description: "FE destekli klasik UI kütüphanesi",
+    description: "FE destekli, sade ve klasik bir GUI kütüphanesi.",
+    image: "/kavoui.png",
+    repo: "https://github.com/Kavo-UI/Kavo-UI-Library",
     code: `loadstring(game:HttpGet("https://raw.githubusercontent.com/Kavo-UI/Kavo-UI-Library/main/source.lua"))()`
   },
   {
     name: "WindUI",
-    description: "Modern ve sade tasarıma sahip",
+    description: "Modern animasyonlu tasarım. Hafif ve sade yapı.",
+    image: "/windui.png",
+    repo: "https://windhub.xyz",
     code: `loadstring(game:HttpGet("https://windhub.xyz/loader.lua"))()`
   },
   {
     name: "Starlight",
-    description: "Gelişmiş tema ve animasyon destekli",
+    description: "Tema destekli, gelişmiş GUI framework'ü.",
+    image: "/starlight.png",
+    repo: "https://github.com/Nebula-Softworks/Starlight",
     code: `loadstring(game:HttpGet("https://nebula-softworks.github.io/starlight.js"))()`
   }
 ];
